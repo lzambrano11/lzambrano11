d74afe594b84cec7c84fa3f6a23c16eb9cbf2679
zambrano <lzambranoutm@gmail.com>
2023-10-28 15:06:29 -0500

    Commit 1

--- src/main/java/com/mycompany/lzambrano11/MainClass.java
+++ src/main/java/com/mycompany/lzambrano11/MainClass.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.lzambrano11;
+package com.mycompany.simplified_math_game;
 
-/**
- *
- * @author LENOVO
- */
 public class MainClass {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+   
+        
     }
-    
 }
+
--- src/main/java/com/mycompany/lzambrano11/MathGame.java
+++ src/main/java/com/mycompany/lzambrano11/MathGame.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.lzambrano11;
+package com.mycompany.simplified_math_game;
 
-/**
- *
- * @author LENOVO
- */
 public class MathGame {
+   
+    
+    
     
 }
+
