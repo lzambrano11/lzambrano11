e1de37dc2ebd03339dfd79b11e24894dc766afa3
zambrano <lzambranoutm@gmail.com>
2023-10-28 15:08:56 -0500

    Commit 2 parte lógica

--- src/main/java/com/mycompany/lzambrano11/MathGame.java
+++ src/main/java/com/mycompany/lzambrano11/MathGame.java
@@ -1,9 +1,19 @@
 package com.mycompany.simplified_math_game;
 
+import java.util.Random;
+
 public class MathGame {
-   
-    
-    
-    
+    private Random random = new Random();
+
+    public int[] generateOperands() {
+        int operand1 = random.nextInt(10) + 1;
+        int operand2 = random.nextInt(10) + 1;
+        return new int[]{operand1, operand2};
+    }
+
+    public String generateQuestion(int operand1, int operand2) {
+        return "¿Cuánto es " + operand1 + " + " + operand2 + "?";
+    }
 }
+
 
