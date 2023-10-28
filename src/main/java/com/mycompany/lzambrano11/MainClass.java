fcd26af66c960e5e63f4ea0689afd2aa85137733
zambrano <lzambranoutm@gmail.com>
2023-10-28 16:02:31 -0500

    Commit 7 Si el porcentaje de respuestas correctas

--- src/main/java/com/mycompany/lzambrano11/MainClass.java
+++ src/main/java/com/mycompany/lzambrano11/MainClass.java
@@ -4,34 +4,49 @@ import java.util.Scanner;  // Importante para el nuevo Scanner
 
 public class MainClass {
     public static void main(String[] args) {
-        QuestionGenerator qg = new QuestionGenerator();
-        UserInterface ui = new UserInterface();
+        
+        Scanner scanner = new Scanner(System.in);  // Este scanner se usará para reiniciar el juego y para elegir la operación
+        String continuePlaying;
 
-        int correctAnswers = 0;
-        int totalQuestions = 0;
+        do {
+            QuestionGenerator qg = new QuestionGenerator();
+            UserInterface ui = new UserInterface();
+            
+            int correctAnswers = 0;
+            int totalQuestions = 0;
 
-        System.out.println("Elige el tipo de operación que quieres practicar: ");
-        System.out.println("1: Suma");
-        System.out.println("2: Resta");
-        System.out.println("3: Multiplicación");
-        int operationType = new Scanner(System.in).nextInt();  // Aquí se cambió la línea para que el usuario elija el tipo de operación
+            System.out.println("Elige el tipo de operación que quieres practicar: ");
+            System.out.println("1: Suma");
+            System.out.println("2: Resta");
+            System.out.println("3: Multiplicación");
+            int operationType = scanner.nextInt();
 
-        while (correctAnswers < 10) {
-            int[] operands = qg.generateOperands();
-            String question = qg.generateQuestion(operands[0], operands[1], operationType);
+            while (correctAnswers < 10) {
+                int[] operands = qg.generateOperands();
+                String question = qg.generateQuestion(operands[0], operands[1], operationType);
 
-            ui.showQuestion(question);
-            int userAnswer = ui.getUserAnswer();
+                ui.showQuestion(question);
+                int userAnswer = ui.getUserAnswer();
 
-            int correctAnswer = qg.calculateAnswer(operands[0], operands[1], operationType);
+                int correctAnswer = qg.calculateAnswer(operands[0], operands[1], operationType);
 
-            if (userAnswer == correctAnswer) {
-                correctAnswers++;
+                if (userAnswer == correctAnswer) {
+                    correctAnswers++;
+                }
+
+                totalQuestions++;
+            }
+
+            double percentageCorrect = ((double) correctAnswers / totalQuestions) * 100;
+
+            if (percentageCorrect < 75) {
+                System.out.println("Por favor pide ayuda adicional a tu instructor.");
+            } else {
+                System.out.println("Felicidades, estás listo para pasar al siguiente nivel!");
             }
             
-            totalQuestions++;
-        }
-
-        System.out.println("Has respondido correctamente a " + correctAnswers + " de " + totalQuestions + " preguntas.");
+            System.out.println("¿Quieres continuar jugando? (s/n)");
+            continuePlaying = scanner.next();
+        } while (continuePlaying.equalsIgnoreCase("s"));
     }
 }
