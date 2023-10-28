cebf693b3b523ff1e90ff8aae18bfad492674110
zambrano <lzambranoutm@gmail.com>
2023-10-28 15:23:09 -0500

    Commit 4 Creamos un objeto de UserInterface

--- src/main/java/com/mycompany/lzambrano11/MainClass.java
+++ src/main/java/com/mycompany/lzambrano11/MainClass.java
@@ -2,8 +2,26 @@ package com.mycompany.simplified_math_game;
 
 public class MainClass {
     public static void main(String[] args) {
-   
-        
+        QuestionGenerator qg = new QuestionGenerator();
+        UserInterface ui = new UserInterface();
+
+        int correctAnswers = 0;
+        int totalQuestions = 0;
+
+        while (correctAnswers < 10) {
+            int[] operands = qg.generateOperands();
+            String question = qg.generateQuestion(operands[0], operands[1]);
+
+            ui.showQuestion(question);
+            int userAnswer = ui.getUserAnswer();
+
+            if (userAnswer == operands[0] + operands[1]) {
+                correctAnswers++;
+            }
+            
+            totalQuestions++;
+        }
+
+        System.out.println("Has respondido correctamente a " + correctAnswers + " de " + totalQuestions + " preguntas.");
     }
 }
-
---
+++ src/main/java/com/mycompany/simplified_math_game/QuestionGenerator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.simplified_math_game;
+
+/**
+ *
+ * @author LENOVO
+ */
+class QuestionGenerator {
+
+    int[] generateOperands() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
