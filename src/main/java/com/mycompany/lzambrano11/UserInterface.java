4b9e9799fc43dcb2f10c6437503fa825c563fc9e
zambrano <lzambranoutm@gmail.com>
2023-10-28 15:17:42 -0500

    Commit 2 creación clase UserInterface

---
+++ src/main/java/com/mycompany/lzambrano11/UserInterface.java
@@ -0,0 +1,16 @@
+package com.mycompany.simplified_math_game;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private Scanner scanner = new Scanner(System.in);
+
+    public int getUserAnswer() {
+        System.out.println("Ingresa tu respuesta: ");
+        return scanner.nextInt();
+    }
+
+    public void showQuestion(String question) {
+        System.out.println(question);
+    }
+}
